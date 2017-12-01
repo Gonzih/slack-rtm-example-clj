4df95dda38fcde99aa118cbd3c40ffbe1311eae6
Max Gonzih <gonzih@gmail.com>
2017-12-01 14:09:22 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aleph "0.4.1"]
-                 [cheshire "5.6.3"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [aleph "0.4.4"]
+                 [cheshire "5.8.0"]
                  [environ "1.1.0"]]
   :plugins  [[lein-environ "1.1.0"]]
   :main slack-rtm-clj.core)
